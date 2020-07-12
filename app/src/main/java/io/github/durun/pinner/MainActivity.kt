53b7f7ae8aa3e00f04349e7b78cb0d0ba16a9f44
Durun <negiigainuki@gmail.com>
2020-07-12 17:02:55 +0900

    image intent to bytes

--- app/src/main/java/io/github/durun/pinner/MainActivity.kt
+++ app/src/main/java/io/github/durun/pinner/MainActivity.kt
@@ -1,20 +1,26 @@
 package io.github.durun.pinner
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
 import android.provider.CalendarContract
+import android.provider.MediaStore
 import android.util.Log
+import androidx.core.net.toFile
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        when {
-            intent?.action == Intent.ACTION_SEND -> {
+        when (intent?.action) {
+            Intent.ACTION_SEND -> {
                 if ("text/plain" == intent.type) {
                     handleSendText(intent) // Handle text being sent
+                }else if(intent.type?.startsWith("image/") == true) {
+                    handleSendImage(intent)
                 }
             }
             else -> {
@@ -36,6 +42,15 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+    private fun handleSendImage(intent: Intent) {
+        (intent.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as? Uri)?.let {
+            // Update UI to reflect image being shared
+            Log.d(TAG, "received: $it")
+            val bytes = contentResolver.openInputStream(it)?.readBytes()
+            Log.d(TAG, "bytes: ${bytes?.take(10)}")
+        }
+    }
+
     companion object {
         private val TAG = MainActivity::class.java.simpleName
     }

5433d683438187d9476c16574e82573a022a0a12
Durun <negiigainuki@gmail.com>
2020-07-12 15:45:39 +0900

    handleSendText

--- app/src/main/java/io/github/durun/pinner/MainActivity.kt
+++ app/src/main/java/io/github/durun/pinner/MainActivity.kt
@@ -1,11 +1,33 @@
 package io.github.durun.pinner
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        when {
+            intent?.action == Intent.ACTION_SEND -> {
+                if ("text/plain" == intent.type) {
+                    handleSendText(intent) // Handle text being sent
+                }
+            }
+            else -> {
+                // Handle other intents, such as being started from the home screen
+            }
+        }
+    }
+    private fun handleSendText(intent: Intent) {
+        intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
+            // Update UI to reflect text being shared
+            Log.d(TAG, "received: $it")
+        }
+    }
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
     }
 }

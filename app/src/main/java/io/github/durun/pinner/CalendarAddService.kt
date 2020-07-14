155f42aa35eb9a7a1f79796158b053723929c718
Durun <negiigainuki@gmail.com>
2020-07-14 13:04:14 +0900

    fix service

--- app/src/main/java/io/github/durun/pinner/CalendarAddService.kt
+++ app/src/main/java/io/github/durun/pinner/CalendarAddService.kt
@@ -1,26 +1,36 @@
 package io.github.durun.pinner
 
-import android.app.IntentService
+import android.app.Service
 import android.content.Intent
+import android.os.IBinder
 import android.util.Log
 import io.ktor.util.InternalAPI
 
-class CalendarAddService : IntentService(CalendarAddService::class.simpleName) {
+class CalendarAddService : Service() {
     companion object {
         private val TAG = CalendarAddService::class.java.simpleName
     }
 
     @InternalAPI
-    override fun onHandleIntent(intent: Intent?) {
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
         Log.d(TAG, "received intent: $intent")
 
         val event = intent?.getSerializableExtra(CalendarEvent.INTENT_KEY) as? CalendarEvent
-            ?: return
+            ?: return START_STICKY
 
-        runCatching {
-            event.submit(context = application)
-        }.onFailure {
-            Log.d(TAG, it.toString())
-        }
+        Thread(
+            Runnable {
+                runCatching {
+                    event.submit(context = application)
+                }.onFailure {
+                    Log.d(TAG, it.toString())
+                }
+                stopSelf()
+            }).start()
+
+        return START_STICKY
     }
+
+    override fun onBind(intent: Intent?): IBinder? = null
 }
